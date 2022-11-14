647a0d273ce7e5c6a92aaab885402fac0a5eeca0
Galva00 <116543265+Galva00@users.noreply.github.com>
2022-11-14 10:29:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <math.h>
+
 using namespace std;
 
 int main() {
-   return 0;
-}
+    float a=0, b=0, x=0, y=0, z=0;
+    cin>> a >> b >> x >> y;
+    if (x<0 and y>0) {
+        z=a*x - b*y;
+        cout << z <<endl;
+    } else if (x>=0 and y<=0) {
+        float P= pow(x,2);
+        z = a*P - b*y;
+        cout << z <<endl;
+    } else {
+        float P = pow(y,2);
+        z = a*x + b*P;
+        cout << z <<endl;
+    }
+        return 0;
+    }
